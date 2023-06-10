3128c89e755fb996c4a598536b3adc99a60ef824
nicodlz <ndlz@pm.me>
2023-06-10 13:39:51 +0200

    setup wagmi

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { WagmiConfig, createConfig, configureChains } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [polygonMumbai],
+  [publicProvider()]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig config={config}>
+      <App />
+    </WagmiConfig>
+  </React.StrictMode>
+);
